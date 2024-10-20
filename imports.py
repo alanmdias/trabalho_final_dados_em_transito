c6c9b4ac9198c838803fd08f1ca8e8aabfc0dfb5
alanmdias <102918626+alanmdias@users.noreply.github.com>
2024-10-20 14:14:05 -0300

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,12 @@
+import requests
+import time
+from datetime import datetime
+import pandas as pd
+import json
+from typing import Optional
+
+
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+
